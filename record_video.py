f43d036e228de68cff6c2f99e1438a02925304c1
liujin <liuyvjin@qq.com>
2022-07-01 14:36:22 +0800

    add record video

--- record_video.py
+++ record_video.py
@@ -4,11 +4,12 @@ import numpy as np
 from pathlib import Path
 import open3d as o3d
 from KinectCamera import Camera
+import time
 
 BASE_PATH = Path(__file__).resolve().parent
 IMG_PATH = BASE_PATH / 'images'
 
-# init record img id
+# init first img id
 def increment_path(path, name='color_', suffix='png'):
     if not path.exists():
         path.mkdir()
@@ -16,7 +17,9 @@ def increment_path(path, name='color_', suffix='png'):
         if not os.path.exists(f'{path}\{name}{n:03d}.{suffix}'):  #
             break
     return n
-IMG_ID = increment_path(IMG_PATH, name = f'color_')
+
+IMG_ID = increment_path(IMG_PATH, name = f'color_')  # first id
+PAUSE_FLAG = False
 
 # save data
 def save_data(color_img, depth_img, pts, id):
@@ -25,15 +28,15 @@ def save_data(color_img, depth_img, pts, id):
     np.save(f'{IMG_PATH}\pc_{id:03d}.npy', pts)
     print(f'Saved at {IMG_PATH}\color_{id:03d}')
 
-#  click mouse mid button to save img
-def onMouse(event, x, y, flags, param):
-    global IMG_ID
-    if event == cv2.EVENT_MBUTTONDOWN:
-        save_data(color_img, depth_img, pts, IMG_ID)
-        IMG_ID += 1
 
+#  click mouse mid button to pause
+def onMouse(event, x, y, flags, param):
+    global PAUSE_FLAG
+    if event == cv2.EVENT_MBUTTONDOWN:
+        PAUSE_FLAG ^= True
 
 if __name__ == '__main__':
+    last_time = time.time()
     cam = Camera()
     cv2.namedWindow('Kinect', 0)
     cv2.setMouseCallback('Kinect', onMouse)
@@ -45,24 +48,21 @@ if __name__ == '__main__':
         cv2.imshow('Kinect', images)
         c = cv2.waitKey(1)
 
-        # open3d
-        # ptsvis = pts.reshape(-1, 3)
-        # ptsvis = pts[np.all(np.isfinite(ptsvis), -1)]
-        # pcd = o3d.geometry.PointCloud()
-        # pcd.points = o3d.utility.Vector3dVector(ptsvis)
-        # o3d.visualization.draw_geometries([pcd])
+        # record img
+        if time.time() - 5 > last_time and not PAUSE_FLAG:
+            last_time = time.time()
+            save_data(color_img, depth_img, pts, IMG_ID)
+            IMG_ID += 1
 
+        # key control
         if c == 27 or c == ord('q'):
             cv2.destroyAllWindows()
             exit()
+
         if c in [ord('r'), ord('R'), 13, 32]:
             save_data(color_img, depth_img, pts, IMG_ID)
             IMG_ID += 1
 
-        # if c in [ord('f'), ord('F')]:
-        #     cam.post_process ^= True
-        #     print("Filter ", "ON" if cam.post_process else "OFF")
-            # img = cv2.imread(depth_path, -1)
 
 
 
